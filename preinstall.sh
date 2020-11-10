2fab3c3bae9faaa3d9cc4d75d805c7d6d7b24946
Jon der 🇨🇳👑 <123.jonathan@gmail.com>
2020-11-10 03:45:19 +0000

    Update

--- preinstall.sh
+++ preinstall.sh
@@ -2,14 +2,14 @@
 
 sudo apt install -y zsh tmux neovim python3-neovim
 
-$ZSH = $(which zsh)
+ZSH=$(which zsh)
 if [[ "$SHELL" != "$ZSH" ]]; then
  chsh -s $ZSH
 fi
 
-if [ ! -d "$HOME/.pyenv" ]; then
- git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-fi
+#if [ ! -d "$HOME/.pyenv" ]; then
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+#fi
 
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
